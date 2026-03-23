d0b08756d2e13780305e2275edfd2de8f0193073
vikas chaurasiya <chaurasiavikas1998@gmail.com>
2026-03-23 18:43:20 +0530

    Implement total_salary calculation to pass test

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -2,7 +2,6 @@ class Salary < ApplicationRecord
   belongs_to :employee
 
   before_save :calculate_total
-
   validates :base_salary, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def calculate_total
